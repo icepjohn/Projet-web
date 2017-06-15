012ef0b51dc4c2564361bdfb18ab042973098a00
moi <moi@moi.com>
2017-06-15 09:50:06 +0200

    Application du framework Bootstrap au gabarit

--- src/views/gabarit.php
+++ src/views/gabarit.php
@@ -4,12 +4,25 @@
 <head>
     <title>
         <?= $pageTitle ?>
+
+        <!-- Chargement du CSS de Bootstrap -->
+        <link rel="stylesheet" href="dependencies/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="dependencies/bootstrap/dist/css/bootstrap-theme.min.css">
     </title>
     <meta charset="utf8">
 </head>
-<body>
-    
-    <?= $content ?>
+<body class="container-fluid">
+
+    <!-- Contenu de l'application -->
+    <section class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <?= $content ?>
+        </div>
+    </section>
+
+    <script src="dependencies/jquery/dist/jquery.min.js"></script>
+    <script src="dependencies/bootstrap/dist/js/bootstrap.min.js"></script>
+
 
 </body>
 </html>
