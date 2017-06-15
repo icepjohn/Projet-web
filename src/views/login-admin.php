6c6df551221d5f935096f81213bcdb1b229bc2e9
moi <moi@moi.com>
2017-06-15 11:50:27 +0200

    Traitement du formulaire d'authentification admininstrateur

--- src/views/login-admin.php
+++ src/views/login-admin.php
@@ -1,8 +1,19 @@
 
+<!-- Affichage des erreurs -->
+<?php if(count($errors) >0): ?>
+    <div class="alert alert-danger">
+        <ul>
+            <?php foreach($errors as $item) :?>
+                <li> <?=$item?> </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
+
 <form method="post">
     <div class="form-group">
         <label>Votre identifiant</label>
-        <input type="text" name="login" class="form-control">
+        <input type="text" name="login" class="form-control" value="<?=$login?>">
     </div>
 
     <div class="form-group">
@@ -11,6 +22,6 @@
     </div>
 
     <div class="form-group">
-        <button type="submit" name="submit">Valider</button> 
+        <button type="submit" name="submit" class="btn btn-primary">Valider</button> 
     </div>
 </form>
