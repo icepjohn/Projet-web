5e7b6db9c2b606cc7ade7dd71bb50f21cdf182a9
moi <moi@moi.com>
2017-06-15 10:22:48 +0200

    Mise en place d'une barre de navigation

--- src/views/gabarit.php
+++ src/views/gabarit.php
@@ -2,16 +2,42 @@
 
 <html>
 <head>
-    <title>
-        <?= $pageTitle ?>
+    <title><?= $pageTitle ?></title>
+    <!-- Chargement du CSS de Bootstrap -->
+    <link rel="stylesheet" href="dependencies/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dependencies/bootstrap/dist/css/bootstrap-theme.min.css">
 
-        <!-- Chargement du CSS de Bootstrap -->
-        <link rel="stylesheet" href="dependencies/bootstrap/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="dependencies/bootstrap/dist/css/bootstrap-theme.min.css">
-    </title>
     <meta charset="utf8">
 </head>
 <body class="container-fluid">
+
+    <!-- Navigation principale -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Projet web</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+                
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Link</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 
     <!-- Contenu de l'application -->
     <section class="row">
@@ -22,6 +48,7 @@
 
     <script src="dependencies/jquery/dist/jquery.min.js"></script>
     <script src="dependencies/bootstrap/dist/js/bootstrap.min.js"></script>
+
 
 
 </body>
